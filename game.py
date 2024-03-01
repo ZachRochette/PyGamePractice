f070ac88789fe77e8b73d85ff6fe587a7cbf060d
ZachRochette <zachrochette@hotmail.com>
2024-03-01 02:19:50 -0700

    commit 1

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import sys
 import pygame
 
+from scripts.entities import PhysicsEntity
+from scripts.utils import load_image
+
 
 class Game:
     def __init__(self):
@@ -12,15 +15,35 @@ class Game:
 
         self.clock = pygame.time.Clock()
 
-        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+        self.movement = [False, False]
+
+        self.assets = {
+            'player': load_image('entities/player.png')
+        }
+
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
 
     def run(self):
         while True:
-            self.screen.blit(self.img, (100, 200))
+            self.screen.fill((14, 219, 248))
+
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.screen)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
 
             pygame.display.update()
             self.clock.tick(60)
